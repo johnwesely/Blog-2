71efc25a87f795687c2c2e60d3f937a45429a709
John Wesely <johnpwesely@gmail.com>
2022-03-02 08:43:09 -0700

    fixed profile picture bug on show view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,7 +9,7 @@
                 </p>
 
                 <div class="flex items-center lg:justify-center text-sm mt-4">
-                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
+                    <img src="{{ asset('storage/' . $post->author->profile_image) }}" alt="profile image thumbnail">
                     <div class="ml-3 text-left">
                         <h5 class="font-bold">
                             <a href="/?author={{ $post->author->username }}">{{ $post->author->name }}</a>
